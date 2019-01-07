c6292720a4b62047725613437b8dbe1809627f32
Jay Rajani <jay.h.rajani@gmail.com>
2019-01-07 20:49:23 +0100

    Fix Replay of Sagas in case of restart

--- mb-transactions/src/main/java/io/blueharvest/labs/axon/transactions/TransactionApplication.java
+++ mb-transactions/src/main/java/io/blueharvest/labs/axon/transactions/TransactionApplication.java
@@ -2,8 +2,10 @@ package io.blueharvest.labs.axon.transactions;
 
 import org.axonframework.config.Configuration;
 import org.axonframework.config.ConfigurationScopeAwareProvider;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.deadline.DeadlineManager;
 import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,17 @@ public class TransactionApplication {
     @Bean
     public DeadlineManager deadlineManager() {
         return SimpleDeadlineManager.builder().scopeAwareProvider(new ConfigurationScopeAwareProvider(axonConfiguration)).build();
+    }
+
+    @Autowired
+    public void configure(EventProcessingConfigurer eventProcessingConfigurer) {
+        eventProcessingConfigurer.registerTrackingEventProcessor(
+                "MoneyTransactionSagaProcessor",
+                Configuration::eventStore,
+                configuration -> TrackingEventProcessorConfiguration
+                        .forSingleThreadedProcessing()
+                        .andInitialTrackingToken(
+                                streamableMessageSource -> streamableMessageSource.createHeadToken()));
     }
 
     public static void main(String[] args) {
